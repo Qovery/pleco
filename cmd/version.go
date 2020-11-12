365f15e8f5470c157ca4f8a4dfd58df72ae5eb42
Pierre Mavro <pmavro@qovery.com>
2020-11-12 02:04:35 +0100

    feat: add github action workflow

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(GetCurrentVersion())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func GetCurrentVersion() string {
+	return "0.1" // ci-version-check
+}
