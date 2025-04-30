d2be8ac9a7d9fa781dec2cada70267113176ae1d
Guillaume Dubroeucq <gdubroeucq@qovery.com>
2025-04-30 08:57:39 +0000

    feat(azure): fix azure rate limit

--- pkg/azure/rg.go
+++ pkg/azure/rg.go
@@ -28,7 +28,7 @@ func DeleteExpiredRGs(sessions AzureSessions, options AzureOptions) {
 		// Fetch the next page of resource groups
 		page, err := pager.NextPage(ctx)
 		if err != nil {
-			log.Errorf("Error listing clusters, error: %v", err)
+			log.Errorf("Error listing resource group, error: %v", err)
 			return
 		}
 
@@ -37,28 +37,28 @@ func DeleteExpiredRGs(sessions AzureSessions, options AzureOptions) {
 			// Check if resource group has a TTL tag
 			ttlStr, ok := group.Tags["ttl"]
 			if !ok || strings.TrimSpace(*ttlStr) == "" {
-				log.Info(fmt.Sprintf("No ttl label value found, ignoring this cluster (`%s`)", *group.Name))
+				log.Info(fmt.Sprintf("No ttl label value found, ignoring this resource group (`%s`)", *group.Name))
 				continue
 			}
 
 			// Parse the TTL value to int64
 			ttl, err := strconv.ParseInt(*ttlStr, 10, 64)
 			if err != nil {
-				log.Warn(fmt.Sprintf("ttl label value `%s` is not parsable to int64, ignoring this cluster (`%s`)", *ttlStr, *group.Name))
+				log.Warn(fmt.Sprintf("ttl label value `%s` is not parsable to int64, ignoring this resource group (`%s`)", *ttlStr, *group.Name))
 				continue
 			}
 
 			// Check if resource group has a creation_date tag
 			creationTimeStr, ok := group.Tags["creation_date"]
 			if !ok || strings.TrimSpace(*creationTimeStr) == "" {
-				log.Info(fmt.Sprintf("No creation_date label value found, ignoring this cluster (`%s`)", *group.Name))
+				log.Info(fmt.Sprintf("No creation_date label value found, ignoring this resource group (`%s`)", *group.Name))
 				continue
 			}
 
 			// Parse the creation_date value to int64 (Unix timestamp)
 			creationTimeInt64, err := strconv.ParseInt(*creationTimeStr, 10, 64)
 			if err != nil {
-				log.Warn(fmt.Sprintf("creation_date label value `%s` is not parsable to int64, ignoring this cluster (`%s`)", *ttlStr, *group.Name))
+				log.Warn(fmt.Sprintf("creation_date label value `%s` is not parsable to int64, ignoring this resource group (`%s`)", *ttlStr, *group.Name))
 				continue
 			}
 
@@ -76,7 +76,7 @@ func DeleteExpiredRGs(sessions AzureSessions, options AzureOptions) {
 				log.Info(fmt.Sprintf("Resource Group `%s` will be deleted", *group.Name))
 				continue
 			}
-
+			
 			// Delete the expired resource group
 			log.Info(fmt.Sprintf("Deleting resource group `%s` created at `%s` UTC (TTL `{%d}` seconds)", *group.Name, creationTime.UTC(), ttl))
 			if _, err := sessions.RG.BeginDelete(ctx, *group.Name, nil); err != nil {
